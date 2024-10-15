722dae7ebc063dc503691da721b0165062ff58f8
strzelczykk <strzelczykk_nat@o2.pl>
2024-10-15 12:42:21 +0200

    tst2

---
+++ laboratorium1/Controllers/BirthController.cs
@@ -0,0 +1,29 @@
+﻿using laboratorium1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laboratorium1.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result([FromForm]Birth model)
+        {
+            if(!model.IsValid())
+            {
+                return View("Error", model);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Form() 
+        {
+            return View();
+        }
+    }
+}
---
+++ laboratorium1/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using laboratorium1.Models;
+using Microsoft.AspNetCore.Mvc;
+using static laboratorium1.Controllers.HomeController;
+
+namespace laboratorium1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Result([FromForm]Calculator calculator)
+        {
+            if(!calculator.IsValid())
+            {
+                return View("Error", calculator);
+            }
+         
+            return View(calculator);
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
