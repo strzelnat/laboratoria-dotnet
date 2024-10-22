d3237c8a0640d99a750ff7492d6b05a969b15bb2
strzelczykk <strzelczykk_nat@o2.pl>
2024-10-22 18:59:49 +0200

    test ->  Edit

---
+++ laboratorium1/Controllers/ContactController.cs
@@ -0,0 +1,88 @@
+﻿using laboratorium1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laboratorium1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // rozw tymczasowe 
+
+        private static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>()
+        {
+           
+        };
+
+        //lista kontaktów
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        //dodawanie kontaktu / formularz
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] //tu zwracam dane z formularza i zapisanie  w kontaktach
+        public IActionResult Add(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+
+                int id = _contacts.Keys.Count != 0 ? _contacts.Keys.Max() : 0;
+                contact.Id = id + 1;
+                _contacts.Add(contact.Id, contact);
+
+                return RedirectToAction("Index",_contacts);
+            }
+
+            return View(contact);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            if(_contacts.ContainsKey(id))
+            {
+                _contacts.Remove(id);
+                return RedirectToAction("Index",_contacts);
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if(_contacts.ContainsKey(id))
+            {
+                return View(_contacts[id]);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[contact.Id] = contact;
+                return RedirectToAction("Index", _contacts);
+            }
+
+            return View(contact);
+        }
+
+
+    }
+}
---
+++ laboratorium1/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace laboratorium1.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [MaxLength(20,ErrorMessage = "Name cannot be longer than 50 charachters.")]
+        [Required(ErrorMessage = "You are obligated to write you're name.")]
+        public string FirstName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Surname cannot be longer than 50 charachters.")]
+        [Required(ErrorMessage = "You are obligated to write you're surname.")]
+        public string LastName { get; set; }
+
+        [RegularExpression(".+\\@.+.[a-z]{2,3}")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Please write phone number correctly.")]
+        public string Telephone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateOnly BirthDate { get; set; }
+
+    }
+}
