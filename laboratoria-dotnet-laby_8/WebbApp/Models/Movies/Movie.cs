d3cd6c36e966689ec34d7121250c89d0d2ab7b9e
TypicalMonkey <pancerny677@interia.pl>
2024-12-19 17:02:44 +0100

    Dodano projekt laby_8+

---
+++ laboratoria-dotnet-laby_8/WebbApp/Models/Movies/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebbApp.Models.Movies;
+
+public partial class Movie
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int  MovieId { get; set; }
+
+    public string? Title { get; set; }
+
+    public int? Budget { get; set; }
+
+    public string? Homepage { get; set; }
+
+    public string? Overview { get; set; }
+
+    public double? Popularity { get; set; }
+
+    public DateOnly? ReleaseDate { get; set; }
+
+    public long? Revenue { get; set; }
+
+    public int? Runtime { get; set; }
+
+    public string? MovieStatus { get; set; }
+
+    public string? Tagline { get; set; }
+
+    public double? VoteAverage { get; set; }
+
+    public int? VoteCount { get; set; }
+ 
+}
